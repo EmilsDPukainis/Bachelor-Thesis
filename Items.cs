d8794da05796d9c26f1384cb6ae6407df7033c77
EmilsDPukainis <epukainis80@gmail.com>
2024-05-19 15:25:40 +0300

    Second

--- Items.cs
+++
@@ -1,9 +0,0 @@
-namespace Shared.Models
-{
-    public class Item
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
